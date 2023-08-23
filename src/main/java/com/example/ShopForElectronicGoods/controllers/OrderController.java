99fc7ccb4f9c094d21f42a9ee2ad248f23581b90
Sinisa Gromovic <gromso97@gmail.com>
2023-08-23 18:21:45 +0200

    "Create the order service and controller"

---
+++ src/main/java/com/example/ShopForElectronicGoods/controllers/OrderController.java
@@ -0,0 +1,54 @@
+package com.example.ShopForElectronicGoods.controllers;
+
+import com.example.ShopForElectronicGoods.models.Order;
+import com.example.ShopForElectronicGoods.services.OrderService;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+
+    @Autowired
+    private OrderService orderService;
+
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(@PathVariable final Integer orderId){
+        Order orderById = orderService.getOrderById(orderId);
+        return ResponseEntity.ok(orderById);
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<Order>> getAllOrders(){
+        List<Order> listOrders = orderService.getOrderList();
+        return ResponseEntity.ok(listOrders);
+    }
+
+    @PostMapping("/addOrder/{cartId}")
+    public ResponseEntity<Order> addOrder(@RequestBody Order order,
+                                          @PathVariable  final Integer cartId){
+        Order order2 = orderService.addOrderByCartId(order, cartId);
+        return ResponseEntity.ok(order2);
+    }
+
+    @PutMapping("/editOrder/{orderId}")
+    public ResponseEntity<Order> editOrder(@RequestBody Order order,
+                                           @PathVariable final Integer orderId){
+        Order order2 = orderService.editOrder(order,orderId);
+        return ResponseEntity.ok(order2);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public void deleteOrder(@PathVariable final Integer orderId){
+       orderService.deleteOrderById(orderId);
+    }
+
+
+}
--- src/main/java/com/example/ShopForElectronicGoods/services/OrderService.java
+++ src/main/java/com/example/ShopForElectronicGoods/services/OrderService.java
@@ -1,8 +1,17 @@
 package com.example.ShopForElectronicGoods.services;
 
+import com.example.ShopForElectronicGoods.Exception.ApiRequestException;
+import com.example.ShopForElectronicGoods.models.ENUMS.OrderStatusEnum;
+import com.example.ShopForElectronicGoods.models.Order;
+import com.example.ShopForElectronicGoods.repository.CartRepository;
 import com.example.ShopForElectronicGoods.repository.OrderRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class OrderService {
@@ -10,5 +19,47 @@ public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+
+    public Order getOrderById(Integer orderId){
+        return orderRepository.findById(orderId).orElseThrow(()
+                -> new ApiRequestException("Order by id "+ orderId + " not found", HttpStatus.NOT_FOUND));
+    }
+
+    public List<Order> getOrderList(){
+        return orderRepository.findAll();
+    }
+
+
+    public Order addOrderByCartId(Order order, final Integer cartId){
+        Order order2 = cartRepository.findById(cartId).map(cart -> {
+            order.setCart(cart);
+           return orderRepository.save(order);
+        }).orElseThrow(() -> new ApiRequestException("Cart by " + cartId + " not Found", HttpStatus.NOT_FOUND));
+        return orderRepository.save(order2);
+    }
+
+
+    public Order editOrder(Order order, final Integer orderId){
+        Order order2 = getOrderById(orderId);
+
+        if(Objects.nonNull(order.getStatus())){
+            try{
+                OrderStatusEnum status = order.getStatus();
+                order2.setStatus(status);
+            }catch (IllegalArgumentException e){
+                e.printStackTrace();
+            }
+        }
+        return orderRepository.save(order2);
+    }
+
+    public void deleteOrderById(final Integer orderId){
+        orderRepository.deleteById(orderId);
+    }
+
 
 }
