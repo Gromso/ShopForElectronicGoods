526166318108d8121d60a037385eb6bca12b71cc
Sinisa Gromovic <gromso97@gmail.com>
2023-08-24 20:15:51 +0200

    ""

--- src/main/java/com/example/ShopForElectronicGoods/configuration/WebMvcConfig.java
+++ src/main/java/com/example/ShopForElectronicGoods/configuration/WebMvcConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
 
-     private final String URL = "D:\\java_programs\\projekti\\storage\\photos";
+     private final String URL ="D:\\java_programs\\projekti\\storage\\photos";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
